06e8fc26a253c83b38e4b6be3bf86bc6c08fed1d
student3 <student3@jh-mbair-158-10.d214.org>
2015-11-06 14:34:58 -0600

    made new view controller

---
+++ intergalacticTravel/ImageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ImageViewController.swift
+//  intergalacticTravel
+//
+//  Created by student3 on 11/6/15.
+//  Copyright © 2015 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
