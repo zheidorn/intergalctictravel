abbab84cb8907f14b298b698caa83f4e5c4239c8
student3 <student3@jh-mbair-158-10.d214.org>
2015-11-09 14:46:35 -0600

    add background pictures with image view

--- intergalacticTravel/ImageViewController.swift
+++ intergalacticTravel/ImageViewController.swift
@@ -10,15 +10,19 @@ import UIKit
 
 class ImageViewController: UIViewController {
     var isBlueStar: Bool?
+    
+    @IBOutlet weak var starImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         if isBlueStar!
         {
             self.view.backgroundColor = UIColor.blueColor()
+            starImageView.image = UIImage(named: "blueStar")
         }
         else
         {
             self.view.backgroundColor = UIColor.redColor()
+            starImageView.image = UIImage(named: "redDwarf")
         }
     }
 
